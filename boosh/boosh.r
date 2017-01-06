3deaa9ed86e55d2a6acccbb11b92bf92e0697828
lefft <tjleffel@gmail.com>
2017-01-05 20:50:51 -0600

    just adding more notes to self (again)

--- boosh/boosh.r
+++ boosh/boosh.r
@@ -21,10 +21,22 @@ boosh::choozeRound(3.45, d=0)
 
 install_github("lefft/boosh")
 
+# to push changes to master branch
+# 
+# first: git add
+# 
+# then: git commit <filename-with-path>
+# 
+# then:
+# for commit message, when in editor:
 # press "i"
 # write your merge message
 # press "esc"
 # write ":wq"
 # then press enter
+#
+# then:
+# use this after commit, to sync to github
+# git push origin master
 
 # blaow another edit
