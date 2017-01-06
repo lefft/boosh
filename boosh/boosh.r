8ceca4c2b75422928bc46b5697a546b166586763
lefft <tjleffel@gmail.com>
2017-01-05 20:22:24 -0600

    okai first commit on boosh

---
+++ boosh/boosh.r
@@ -0,0 +1,22 @@
+# boosh.r -- back of envelope area for interfacing w the boosh package
+
+# g
+library("devtools")
+library("roxygen2")
+setwd("../")
+dir()
+create("boosh")
+
+
+setwd("boosh")
+document()
+
+dir()
+setwd("../")
+install("boosh")
+
+
+boosh::azChar(345)
+boosh::choozeRound(3.45, d=0)
+
+install_github("lefft/boosh")
