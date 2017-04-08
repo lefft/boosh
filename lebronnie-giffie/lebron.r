57af1f43b5b5c7c566a957ed56de74b34616726e
lefft <tjleffel@gmail.com>
2017-04-08 16:23:04 -0500

    messing around w gifs

---
+++ lebronnie-giffie/lebron.r
@@ -0,0 +1,53 @@
+
+
+## going to:
+##    make a gif from a set of pics w r imagemagick plugin
+##    make same gif from same pics w python imagemagick plugin
+
+### r example
+library("magick")
+library("magrittr")
+
+  dir()[endsWith(dir(), ".png")] %>% 
+  image_read() %>% 
+  image_crop("1200x1600+750") %>% 
+  image_join() %>% 
+  image_animate(fps=2) %>% 
+  image_write(path="out/bronnie.gif", format="gif", quality=50, flatten=FALSE)
+
+# shd be 1300x1600 result a.r.
+# image_crop("100x150+50") # crop out width:100px and height:150px starting +50px from the left
+
+bron <- image_read("swh_swatch_1.jpg")
+
+for (i in seq_along(pix)){
+  bron <- image_composite(bron, image_read(pix[i]))
+}
+
+
+### python example
+
+
+
+
+# these dont work :(
+# image_write(bron, "bron-just-composite.gif")
+# image_write(image_join(bron), "bron-joined.gif")
+# image_write(image_animate(image_join(bron)), "bron-joined-animate.gif")
+# image_write(image_animate(bron), "bron-animated.gif")
+getwd()
+system("ls")
+
+
+system("magick convert *.png -delay 3 -loop 0 boosh.gif")
+# demo code:
+	# logo <- image_read("https://www.r-project.org/logo/Rlogo.png")
+	# banana <- image_scale(image_read("https://jeroen.github.io/images/banana.gif"), "150")
+	# background <- image_background(image_scale(logo, "200"), "white", flatten = TRUE)
+	# frames <- lapply(banana, function(frame) {
+	#   image_composite(background, frame, offset = "+70+30")
+	# })
+	# animation <- image_animate(image_join(frames))
+	# print(animation)
+	# image_write(animation, "blaowwie.gif")
+
