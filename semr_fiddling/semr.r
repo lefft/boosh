4c4917dc27635242ef5eba58e25d0f0de366e642
lefft <tjleffel@gmail.com>
2017-05-09 19:45:53 -0500

    make playground for building lil fragments

---
+++ semr_fiddling/semr.r
@@ -0,0 +1,103 @@
+# === === === === === === === 
+#  [notes in: readme.md]  === 
+# === === === === === === === 
+
+### 0. load functions + dependencies ##########################################
+# === === === === === === === === === === === === === === === === === === 
+source("functions.r")
+
+### 1. bare-bones extensional semantics #######################################
+# === === === === === === === === === === === === === === === === === === 
+
+### __semantics for constants #########
+# === === === === === ===  
+# 
+# noun and name meanings
+dog  <- make_cnoun(DOG)
+cat  <- make_cnoun(CAT)
+john <- make_pname(JOHN)
+mary <- make_pname(MARY)
+# verb meanings 
+laughed <- make_itv(LAUGHED)
+barked  <- make_itv(BARKED)
+purred  <- make_itv(PURRED)
+ran     <- make_itv(RAN)
+chased  <- make_tv(CHASED)
+bit     <- make_tv(BIT)
+gave    <- make_itv(GAVE)
+# logical connective meanings
+not    <- function(p)             !p 
+and    <- function(q) function(p)  p & q
+or     <- function(q) function(p)  p | q
+ifthen <- function(q) function(p) !p | q
+# determiner meanings
+every <- make_det(condition=all)
+a     <- make_det(condition=any)
+two   <- make_det(condition=function(x)sum(x)>1)
+the <- function(P){
+  if (length(P)!=1){return("presupfail!")}
+  return(paste0("the_", P))
+}
+# 
+### end lexical semantics
+# === === === === === ===  
+
+### __toy model specification #########
+# === === === === === ===  
+#
+# noun extensions
+DOG    <- c("fido","spot","sparky")
+CAT    <- c("felix","garfield")
+PERSON <- c("john","mary")
+THING  <- c(DOG, CAT, PERSON)
+# name extensions
+JOHN <- "john"
+MARY <- "mary"
+# domain of discourse
+D <- c(DOG,CAT,PERSON)
+# intransitive verb extensions
+LAUGHED <- c("john","fido","sparky","garfield")
+BARKED  <- c("fido","sparky","spot")
+PURRED  <- c("felix","garfield")
+RAN     <- c("mary","sparky")
+# transitive verb extensions
+CHASED <- c(ff=c("fido","felix"),syf=c("sparky","felix"),stf=c("spot","felix"))
+BIT    <- c(mj=c("mary","john"),fs=c("sparky","felix"),gj=c("garfield","john"))
+ATE    <- c(ff=c("fido","felix"),gm=c("garfield","mary"))
+# ditransitive verb extensions
+GAVE <- c(jfm=c("john","felix","mary"),msj=c("mary","sparky","john"))
+# 
+### end model specification
+# === === === === === ===  
+
+### testing phase 1:
+#
+# should be true that 'every dog chased a cat' on one reading; not on other
+every(DOG)(BARKED)
+for (x in DOG){print(barked(x))}
+
+
+# missing vectorization in some lexical entry :/ 
+#   every(DOG)(function(x){chased("felix")(x)})
+every(DOG)(THING)
+for (x in DOG){print(barked(x))}
+
+
+# order of args wrong! x shd be direct obj!
+for (x in DOG){print(chased(x)("felix"))}
+
+### 2. basic model theory #####################################################
+# === === === === === === === === === === === === === === === === === === 
+
+# - need to introduce variables
+# - shd be based on gallin ty2 (flexibly!)
+# - keep simple where possible
+# - just use built-ins for booleans (TRUE, FALSE) and NULL
+# - use ent, bool as primitive types + generate functional types recursively
+# - complex types interpreted as function space
+# - an interp is a pair M = (D, V) for D a domain and V a valuation
+# - truth defined relative to an interp plus an assignment (a "model")
+
+
+### 3. simple cfg-based syntax ################################################
+# === === === === === === === === === === === === === === === === === === 
