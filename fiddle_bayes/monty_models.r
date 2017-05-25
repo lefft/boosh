eaf42026de0d63ff3982392d308710285525bd6e
lefft <tjleffel@gmail.com>
2017-05-24 19:50:19 -0500

    start generalizing monty code

---
+++ fiddle_bayes/monty_models.r
@@ -0,0 +1,9 @@
+### playground for trying different parameters on simulate_mh()
+
+# load dependencies
+library("dplyr"); library("ggplot2")
+
+# load functions for simulating mh problem
+source("monty_functions.r")
+
+
